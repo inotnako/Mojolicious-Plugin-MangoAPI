c0afd523def5ce3451db5235cb5510b033756203
Anton Konovalov <antoni.konovalov@gmail.com>
2013-10-07 14:35:13 +0400

    it's work!

--- lib/Mojolicious/Plugin/MangoAPI.pm
+++ lib/Mojolicious/Plugin/MangoAPI.pm
@@ -1,13 +1,174 @@
 package Mojolicious::Plugin::MangoAPI;
 use Mojo::Base 'Mojolicious::Plugin';
 
+use Mango;
+use Mango::BSON  qw/bson_oid/;
+use Data::Dump qw/dump/;
+
 our $VERSION = '0.01';
 
+has cfg => sub { +{} };
+
 sub register {
-  my ($self, $app) = @_;
+  	my ($p, $app, $config) = @_;
+
+	my $auth  = ($config->{user} and $config->{pass} ? $config->{user} . ':' . $config->{pass} . '@' : '' );
+	my $server_db = ($config->{server} and $config->{db} ? $config->{server} . '/' . $config->{db} : '');
+
+ 	return if !$server_db and !$config->{uri};
+
+  	$config->{uri} = $config->{uri} ||  'mongodb://' . $auth . $server_db;
+  	$config->{route} = $config->{route} || $app->routes;
+  	$config->{rest_name} = $config->{rest_name} || 'mango';
+  	$config->{max_connections} = $config->{max_connections} || 10;
+
+  	$p->cfg($config);
+
+	$app->attr( mongodb => sub { 
+    	my $mango = Mango->new($p->cfg->{uri});
+    	$mango->max_connections($p->cfg->{max_connections}) if $p->cfg->{max_connections};
+    	$mango;
+    });
+
+	$app->helper( mango => sub { shift->app->mongodb });
+
+	$p->cfg->{route}->add_shortcut( mango_api => sub {
+		my ($r, $name) = @_;
+
+		return unless $name;
+
+		my $c = $app->mango->db->collection($name);
+		return unless $c;
+
+		my $mango_api = $r->route('/' . $p->cfg->{rest_name} . "/$name");
+
+		#Create new
+		$mango_api->post(sub {
+			my $self = shift;
+			$self->render_later;
+
+			my $params = ($self->req->json || $self->req->params->to_hash );
+
+			$c->insert($params => sub {
+				my ($collection, $err, $oid) = @_;
+
+				if ($err) {
+					$self->render(json => {
+						ok => 0,
+						msg => $err
+					});
+				} else {
+					$self->render(json => {
+						ok => 1,
+						oid => $oid
+					});
+				}
+			});
+		})->name("create_$name");
+
+		#Read by current $oid
+		$mango_api->get('/:oid' => sub {
+			my $self = shift;
+			my $oid = bson_oid( $self->stash('oid') );
+			$self->render_later;
+
+			$c->find_one($oid => sub {
+ 				my ($cursor, $err, $doc) = @_;
+
+ 				if ($err) {
+				 	$self->render(json => {
+				 		ok => 0,
+				 		msg => $err
+				 	});
+				} else {
+				 	$self->render(json => {
+				 		ok => 1,
+				 		data => $doc
+				 	});
+				}
+ 			});
+
+		})->name("find_one_$name");
+
+		# Read all from this collections
+		$mango_api->get(sub {
+			my $self = shift;
+			$self->render_later;
+
+			$c->find->all(sub {
+				my ($cursor, $err, $docs) = @_;
+
+				if ($err) {
+			 		$self->render(json => {
+				 		ok => 0,
+						msg => $err
+				 	});
+				} else {
+				 	$self->render(json => {
+				 		ok => 1,
+				 		data => $docs,
+				 		total => scalar (@$docs)
+				 	});
+				}
+			});
+		})->name("find_$name");
+
+		#Update by current $oid
+		$mango_api->put('/:oid' => sub {
+			my $self = shift;
+			my $oid = bson_oid( $self->stash('oid') );
+			$self->render_later;
+
+			my $params = ( $self->req->json || $self->req->params->to_hash );
+
+			$c->update( ($oid, $params,{single => 1}) => sub {
+				my ($collection, $err, $doc) = @_;
+
+				if ($err) {
+			 		$self->render(json => {
+				 		ok => 0,
+						msg => $err
+				 	});
+				} else {
+				 	$self->render(json => {
+				 		ok => 1,
+				 		data => $doc
+				 	});
+				}
+			});
+
+		})->name("update_one_$name");
+
+		#Delete by current $oid
+		$mango_api->delete('/:oid' => sub {
+			my $self = shift;
+			my $oid = bson_oid( $self->stash('oid') );
+			$self->render_later;
+
+			$c->remove(($oid, {single => 1}) => sub {
+				my ($collection, $err, $doc) = @_;
+
+				if ($err) {
+			 		$self->render(json => {
+				 		ok => 0,
+						msg => $err
+				 	});
+				} else {
+				 	$self->render(json => {
+				 		ok => 1,
+				 		data => $doc
+				 	});
+				}
+			});
+		})->name("remove_one_$name");
+
+		return $mango_api;
+	});
+
 }
 
 1;
+
 __END__
 
 =encoding utf8
--- t/basic.t
+++ t/basic.t
@@ -3,8 +3,16 @@ use Mojo::Base -strict;
 use Test::More;
 use Mojolicious::Lite;
 use Test::Mojo;
+use Time::Piece;
 
-plugin 'MangoAPI';
+use Mojo::IOLoop;
+
+use lib 'lib';
+
+my $test_co = 'test';
+my $text_test = 'testAloxa when time => ' . localtime->hms;
+my $test_oid = $ENV{TEST_OID};
+
 
 get '/' => sub {
   my $self = shift;
@@ -13,5 +21,42 @@ get '/' => sub {
 
 my $t = Test::Mojo->new;
 $t->get_ok('/')->status_is(200)->content_is('Hello Mojo!');
+
+my $app = $t->app;
+my $r = $app->routes;
+
+$app->plugin('MangoAPI',{
+	uri => $ENV{TEST_URI},
+	route => $r
+});
+
+
+$r->mango_api($test_co);
+
+$t->post_ok("/mango/$test_co" => {DNT => 1} => json => { a => $text_test . ' => for A.', b => $text_test . ' => for B.' } )
+	->status_is(200)
+	->json_has({ok => 1}, 'right content');
+
+$t->get_ok("/mango/$test_co")
+	->status_is(200)
+	->json_has({ok => 1}, 'right content');
+
+if ($test_oid) {
+	$t->get_ok("/mango/$test_co/$test_oid")
+		->status_is(200)
+		->json_has({ok => 1}, 'right content');
+
+	$t->put_ok("/mango/$test_co/$test_oid" => { DNT => 1} => json => { a => $text_test . ' => for Ax. Update' . localtime->hms  })
+		->status_is(200)
+		->json_has({ok => 1}, 'right content');
+
+	$t->delete_ok("/mango/$test_co/$test_oid")
+		->status_is(200)
+		->json_has({ok => 1}, 'right content');
+}
+
+# $t->get_ok("/mango/$test_co")
+# 	->status_is(200)
+# 	->json_has({ok => 1}, 'right content');
 
 done_testing();
